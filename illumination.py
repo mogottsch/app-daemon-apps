995369847efcb9612eb00fe8a0449e26de06e0da
root <root@core-ssh.local.hass.io>
2024-05-26 15:45:48 +0200

    Update

--- illumination.py
+++ illumination.py
@@ -3,16 +3,15 @@ import hassapi as hass
 from datetime import datetime
 
 
-class Light(hass.Hass):
-    sensor_occupancy_entity: str = ""
-    sensor_illuminance_entity: str = ""
-    light_entity: str = ""
-    illuminance_threshold: int = 10
-    night_mode_entity: str = ""
-    disabled_entity: Optional[str] = None
-
-    day_illuminance: int = 255
-    night_illuminance: int = 10
+class BathroomCeilingLight(hass.Hass):
+    sensor_occupancy_entity: str
+    light_entity: str
+    night_mode_entity: str
+    chill_mode_entity: str
+    day_illuminance: int
+    night_illuminance: int
+    normal_temeparture: int
+    chill_temperature: int
 
     expected_action = None
 
@@ -24,12 +23,13 @@ class Light(hass.Hass):
 
     def init_values(self) -> None:
         self.sensor_occupancy_entity = self.args["sensor_occupancy"]
-        self.sensor_illuminance_entity = self.args["sensor_illuminance"]
         self.light_entity = self.args["light"]
-        self.illuminance_threshold = int(self.args.get("illuminance_threshold", 10))
         self.night_mode_entity = self.args["night_mode"]
-        self.day_illuminance = self.args.get("day_illuminance")
-        self.night_illuminance = self.args.get("night_illuminance")
+        self.chill_mode_entity = self.args["chill_mode"]
+        self.day_illuminance = self.args["day_illuminance"]
+        self.night_illuminance = self.args["night_illuminance"]
+        self.normal_temperature = self.args["normal_temperature"]
+        self.chill_temperature = self.args["chill_temperature"]
         self.manual_mode_debounce = int(self.args.get("manual_mode_debounce", 180))
         self.disabled_entity = self.args.get("disabled")
 
@@ -37,8 +37,8 @@ class Light(hass.Hass):
 
     def init_listeners(self) -> None:
         self.listen_state(self.update_light_state, self.sensor_occupancy_entity)
-        self.listen_state(self.update_light_state, self.sensor_illuminance_entity)
         self.listen_state(self.update_light_state, self.night_mode_entity)
+        self.listen_state(self.update_light_state, self.chill_mode_entity)
         self.listen_state(self.handle_light_change, self.light_entity)
 
     def is_disabled(self) -> bool:
@@ -54,36 +54,42 @@ class Light(hass.Hass):
         self.log(f"manual action registered on {self.manual_action_registered_on}")
         self.run_in(self.update_light_state, self.manual_mode_debounce)
 
-    def get_illuminance(self) -> int:
-        return int(self.get_state(self.sensor_illuminance_entity))
-
     def get_occupancy(self) -> bool:
         return self.get_state(self.sensor_occupancy_entity) == "on"
 
     def is_night_mode(self) -> bool:
         return self.get_state(self.night_mode_entity) == "on"
 
+    def is_chill_mode(self) -> bool:
+        return self.get_state(self.chill_mode_entity) == "on"
+
     def light_is_on(self) -> bool:
         return self.get_state(self.light_entity) == "on"
 
     def calculate_light_state(self) -> bool:
         if self.is_disabled():
+            self.log("light is disabled")
             return False
         if not self.get_occupancy():
+            self.log("no occupancy")
             return False
-        if self.light_is_on():
-            return True
-        if self.get_illuminance() < self.illuminance_threshold:
-            return True
-        return False
+        return True
 
     def calculate_light_brightness(self) -> int:
         if self.is_night_mode():
             return self.night_illuminance
         return self.day_illuminance
 
+    def calculate_light_temperature(self) -> int:
+        if self.is_chill_mode():
+            return self.chill_temperature
+        return self.normal_temperature
+
     def get_brightness(self, entity_id) -> int:
         return int(self.get_state(entity_id, attribute="brightness"))
+
+    def get_temperature(self, entity_id) -> int:
+        return int(self.get_state(entity_id, attribute="color_temp"))
 
     def update_needed(self, new_light_state) -> bool:
         if (
@@ -91,25 +97,40 @@ class Light(hass.Hass):
             and (datetime.now() - self.manual_action_registered_on).total_seconds()
             < self.manual_mode_debounce
         ):
+            self.log("recent manual action - no update needed")
             return False
 
         if new_light_state != self.light_is_on():
+            self.log("light state mismatch - update needed")
             return True
 
         # further check if brightness adjustment is needed
 
         if not self.light_is_on():
+            self.log("light is off - no need to check for brightness")
             return False
 
-        if not self.day_illuminance or not self.night_illuminance:
+        if self.day_illuminance is None or self.night_illuminance is None:
+            self.log("illuminance not set - no update needed")
             return False
 
         desired_brightess = self.calculate_light_brightness()
-        return desired_brightess != self.get_brightness(self.light_entity)
+        if desired_brightess != self.get_brightness(self.light_entity):
+            self.log("brightness mismatch - update needed")
+            return True
+
+        desired_temperature = self.calculate_light_temperature()
+        if desired_temperature != self.get_temperature(self.light_entity):
+            self.log("temperature mismatch - update needed")
+            return True
+        return False
 
     def update_light_state(self, *_) -> None:
         should_light_on = self.calculate_light_state()
+        self.log(should_light_on)
+        self.log(f"light should be {should_light_on and 'on' or 'off'}")
         if not self.update_needed(should_light_on):
+            self.log("no update needed")
             return
 
         self.log(f"light is set to {should_light_on and 'on' or 'off'}")
@@ -118,8 +139,11 @@ class Light(hass.Hass):
 
         if should_light_on:
             kwargs = {}
-            if self.day_illuminance and self.night_illuminance:
-                kwargs["brightness"] = self.calculate_light_brightness()
+            kwargs["brightness"] = self.calculate_light_brightness()
+            kwargs["color_temp"] = self.calculate_light_temperature()
+            # self.log(
+            #     f"setting light on with brightness {kwargs['brightness']} and temperature {kwargs['color_temp']}"
+            # )
             self.turn_on(self.light_entity, **kwargs)
             return
 
